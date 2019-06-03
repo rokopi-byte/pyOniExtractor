89af5d6ef42c62612dd830919e7598899af82bf1
sinc <rocco.pietrini@gmail.com>
2019-06-03 17:54:35 +0200

    bugfix

--- frame_extractor.py
+++ frame_extractor.py
@@ -15,10 +15,10 @@ def openDevice(video_path):
         dev = openni2.Device.open_file(video_path)
         pbs = openni2.PlaybackSupport(dev)
 
-        pbs.set_repeat_enabled(False)
+        pbs.set_repeat_enabled(True)
         pbs.set_speed(-1.0)
 
-        return dev,pbs
+        return dev, pbs
     except Exception as ex:
         print(ex)
         raise Exception("Initialization Error")
@@ -29,18 +29,31 @@ def processDepth(dev,pbs,interval,dst):
         depth_stream.start()
         depth_scale_factor = 255.0 / (650.0-520.0)
         depth_scale_beta_factor = -520.0*255.0/(650.0-520.0)
-        print("Depth frames: " + str(depth_stream.get_number_of_frames()))
-        for i in range(1,depth_stream.get_number_of_frames()+1,interval):
-            pbs.seek(depth_stream,i)
-            frame_depth = depth_stream.read_frame()
-            frame_depth_data = frame_depth.get_buffer_as_uint16()
-            depth_array = np.ndarray((frame_depth.height, frame_depth.width),dtype=np.uint16,buffer=frame_depth_data)
-            depth_uint8 = depth_array*depth_scale_factor+depth_scale_beta_factor
-            depth_uint8[depth_uint8>255] = 255
-            depth_uint8[depth_uint8<0] = 0
-            depth_uint8 = depth_uint8.astype('uint8')
-            cv2.imwrite(dst + "/" + str(frame_depth.frameIndex) + "_16bit.png",depth_array)
-            cv2.imwrite(dst + "/" + str(frame_depth.frameIndex) + "_8bit.png",depth_uint8)
+        nframes = depth_stream.get_number_of_frames()
+        print("Depth frames: " + str(nframes))
+        with open('timestampsdepth.txt', 'w') as tfile:
+            for i in range(1, nframes+1, interval):
+                try:
+                    if i != nframes:
+                        pbs.seek(depth_stream, i)    #Seek hangs if last frame is empty, sometimes happens
+                except Exception as ex:
+                    print("Error on depth stream seek ", ex)
+                    continue
+                s = openni2.wait_for_any_stream([depth_stream], 2)
+                if s != depth_stream:
+                    print("Error on depth stream, timeout reached reading frame n: ", str(i))
+                    continue
+                frame_depth = depth_stream.read_frame()
+                frame_depth_data = frame_depth.get_buffer_as_uint16()
+                depth_array = np.ndarray((frame_depth.height, frame_depth.width), dtype=np.uint16,
+                                         buffer=frame_depth_data)
+                depth_uint8 = depth_array*depth_scale_factor+depth_scale_beta_factor
+                depth_uint8[depth_uint8 > 255] = 255
+                depth_uint8[depth_uint8 < 0] = 0
+                depth_uint8 = depth_uint8.astype('uint8')
+                cv2.imwrite(dst + "/" + str(frame_depth.frameIndex) + "_16bit.png", depth_array)
+                cv2.imwrite(dst + "/" + str(frame_depth.frameIndex) + "_8bit.png", depth_uint8)
+                tfile.write(str(frame_depth.frameIndex) + ';' + str(frame_depth.timestamp) + '\n')
         depth_stream.close()
         print("All depth frames extracted")
     except Exception as ex:
@@ -50,14 +63,27 @@ def processColor(dev,pbs,interval,dst):
     try:
         color_stream = dev.create_color_stream()
         color_stream.start()
-        print("Color frames: " + str(color_stream.get_number_of_frames()))
-        for i in range(1,color_stream.get_number_of_frames()+1,interval):
-            pbs.seek(color_stream,i)
-            frame_color = color_stream.read_frame()
-            frame_color_data = frame_color.get_buffer_as_uint8()
-            color_array = np.ndarray((frame_color.height, frame_color.width, 3),dtype=np.uint8,buffer=frame_color_data)
-            color_array = cv2.cvtColor(color_array, cv2.COLOR_BGR2RGB)
-            cv2.imwrite(dst + "/" + str(frame_color.frameIndex) + "_color.png",color_array)
+        nframes = color_stream.get_number_of_frames()
+        print("Color frames: " + str(nframes))
+        with open('timestampscolor.txt', 'w') as tfile:
+            for i in range(1, nframes+1, interval):
+                try:
+                    if i != nframes:
+                        pbs.seek(color_stream, i)  # Seek hangs if last frame is empty, sometimes happens
+                except Exception as ex:
+                    print("Error on color stream seek ", ex)
+                    continue
+                s = openni2.wait_for_any_stream([color_stream], 2)
+                if s != color_stream:
+                    print("Error on color stream, timeout reached reading frame n: ", str(i))
+                    continue
+                frame_color = color_stream.read_frame()
+                frame_color_data = frame_color.get_buffer_as_uint8()
+                color_array = np.ndarray((frame_color.height, frame_color.width, 3), dtype=np.uint8,
+                                         buffer=frame_color_data)
+                color_array = cv2.cvtColor(color_array, cv2.COLOR_BGR2RGB)
+                cv2.imwrite(dst + "/" + str(frame_color.frameIndex) + "_color.png", color_array)
+                tfile.write(str(frame_color.frameIndex) + ';' + str(frame_color.timestamp) + '\n')
         color_stream.close()
         print("All color frames extracted")
     except Exception as ex:
@@ -73,15 +99,15 @@ def main():
     dst = args.dst
     if not os.path.exists(dst):
         os.mkdir(dst)
-        print("Directory ",dst ," Created ")
+        print("Directory ", dst, " Created ")
     try:
-        dev,pbs = openDevice(args.video_path.encode('utf-8'))
+        dev, pbs = openDevice(args.video_path.encode('utf-8'))
         if dev.has_sensor(openni2.SENSOR_COLOR):
             print("Color Stream found")
-            processColor(dev,pbs,interval,dst)
+            processColor(dev, pbs, interval, dst)
         if dev.has_sensor(openni2.SENSOR_DEPTH):
             print("Depth Stream found")
-            processDepth(dev,pbs,interval,dst)
+            processDepth(dev, pbs, interval, dst)
         print("Done!")
     except Exception as ex:
         print(ex)
